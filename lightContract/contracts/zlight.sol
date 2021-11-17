5680b27072bf1dda2e5910fdd8965a71f19f47af
hygkui <hygkui@gmail.com>
2021-11-18 01:11:00 +0800

    随机id mint

--- lightContract/contracts/zlight.sol
+++ lightContract/contracts/zlight.sol
@@ -5,21 +5,36 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract ZLight is ERC721, ERC721Enumerable, Ownable {
+contract tLight is ERC721, ERC721Enumerable, Ownable {
+    using Address for address;
     bool public saleIsActive = false;
     string private _baseURIextended;
 
     uint256 public constant MAX_SUPPLY = 50;
     uint256 public constant MAX_PUBLIC_MINT = 47;
-    uint256 public constant PRICE_PER_TOKEN = 0.06 ether;
+    uint256 public constant PRICE_PER_TOKEN = 0.4 ether;
     uint256 public constant RESERVE_AMOUNT = 3;
 
-    constructor() ERC721("ZLight", "ZLLT") {
-      reserveMint(); // 部署时直接预挖
+
+    mapping(uint256 => address) private _owners; // nft owners
+
+    constructor() ERC721("tLight", "tLLT") {
+      reserveMint(RESERVE_AMOUNT); // 部署时直接预挖
     }
   
+    function ownerOfTokens(uint256 tokenId) public view returns(address) {
+        return _owners[tokenId];
+    }
+
+    // 是否已经被mint
+    function exists(uint256 tokenId) public view virtual returns (bool) {
+        return _owners[tokenId] != address(0);
+    }
+
     function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+        require(address(0) == from || _owners[tokenId] == from, "contract owner or have the token ");
         super._beforeTokenTransfer(from, to, tokenId);
+        _owners[tokenId] = to;
     }
 
     function supportsInterface(bytes4 interfaceId) public view virtual override(ERC721, ERC721Enumerable) returns (bool) {
@@ -34,10 +49,12 @@ contract ZLight is ERC721, ERC721Enumerable, Ownable {
         return _baseURIextended;
     }
 
-    function reserveMint() public onlyOwner {
-      uint i;
-      for (i = 0; i < RESERVE_AMOUNT; i++) {
-          _safeMint(msg.sender, i);
+    function reserveMint(uint256 _amount) public onlyOwner {
+      uint256[] memory tokenIds = getRandomNumbers(_amount);
+      for (uint256 i = 0; i < _amount; i++) {
+        uint256 tokenId = tokenIds[i];
+        _owners[tokenId] = msg.sender;
+        _safeMint(msg.sender, tokenId);
       }
     }
 
@@ -48,16 +65,53 @@ contract ZLight is ERC721, ERC721Enumerable, Ownable {
         saleIsActive = newState;
     }
 
-    function mint(uint numberOfTokens) external payable {
+    function mint(uint256 _amount) external payable {
         uint256 ts = totalSupply();
         require(saleIsActive, "Sale must be active to mint tokens");
-        require(numberOfTokens <= MAX_PUBLIC_MINT, "Exceeded max token purchase");
-        require(ts + numberOfTokens <= MAX_SUPPLY, "Purchase would exceed max tokens");
-        require(PRICE_PER_TOKEN * numberOfTokens <= msg.value, "Ether value sent is not correct");
+        require(_amount <= MAX_PUBLIC_MINT, "Exceeded max token purchase");
+        require(ts + _amount <= MAX_SUPPLY, "Purchase would exceed max tokens");
+        require(PRICE_PER_TOKEN * _amount <= msg.value, "Ether value sent is not correct");
 
-        for (uint256 i = 0; i < numberOfTokens; i++) {
-            _safeMint(msg.sender, ts + i);
+        uint256[] memory tokenIds = getRandomNumbers(_amount);
+        for (uint256 i = 0; i < _amount; i++) {
+          uint256 tokenId = tokenIds[i];
+          _owners[tokenId] = msg.sender;
+          _safeMint(msg.sender, tokenId);
         }
+    }
+
+    function getRandomNumbers(uint256 _amount) public view virtual returns (uint256[] memory) {
+      uint256[] memory tokenIds = new uint256[](_amount);
+      uint256 initRand = 0;
+      uint256 each = 0;
+      bytes32 bHash = blockhash(block.number - 1);
+
+      while(initRand != _amount){
+          uint256 randomNumber = uint256(
+              uint256(
+                  keccak256(
+                      abi.encodePacked(
+                          block.timestamp, 
+                          bHash, 
+                          _msgSender(), 
+                          each
+                      )
+                  )
+              ) % MAX_SUPPLY
+          );
+          bool inArray;
+          for(uint256 i = 0; i<initRand; i++){
+              if(randomNumber == tokenIds[i]){
+                  inArray = true;
+              }
+          }
+          if(!exists(randomNumber) && !inArray){
+              tokenIds[initRand] = randomNumber;
+              initRand += 1;
+          }
+          each += 1;
+        }
+      return tokenIds;
     }
 
     function withdraw() public onlyOwner {
--- lightContract/test/token.js
+++ lightContract/test/token.js
@@ -5,11 +5,11 @@ describe('token contract', function () {
   it('should assign the total supply of tokens to the contract owner', async () => {
     const [owner] = await ethers.getSigners();
 
-    const Token = await ethers.getContractFactory("ZLight");
+    const Token = await ethers.getContractFactory("tLight");
 
     const HHToken = await Token.deploy()
     const ownerBalance = await HHToken.balanceOf(owner.address);
-
+    console.log('ownerBalance :>> ', ownerBalance);
     expect(await HHToken.totalSupply()).to.equal(ownerBalance);
   })
 })
@@ -35,10 +35,26 @@ describe('token contract', function () {
 describe('token deploy then reverse mint', function () {
   it('should deploy and mint', async () => {
     const [owner] = await ethers.getSigners();
-    const Token = await ethers.getContractFactory("ZLight");
+    const Token = await ethers.getContractFactory("tLight");
     const ZLightToken = await Token.deploy();
     expect(await ZLightToken.RESERVE_AMOUNT()).to.equal(3);
     expect(await ZLightToken.balanceOf(owner.address)).to.equal(3);
     expect(await ZLightToken.totalSupply()).to.equal(3)
+  })
+})
+
+
+describe('token mint', function () {
+  it('should mint', async () => {
+    const [owner, addr1] = await ethers.getSigners();
+    const Token = await ethers.getContractFactory("tLight");
+    const ZLightToken = await Token.deploy();
+    const pricePerToken = await ZLightToken.PRICE_PER_TOKEN()
+    await ZLightToken.setSaleState(true);
+    const mintAmount = 10
+    await ZLightToken.connect(addr1).mint(mintAmount, {
+      value: pricePerToken.mul(mintAmount),
+    })
+    expect(await ZLightToken.balanceOf(addr1.address)).to.equal(mintAmount);
   })
 })
